620bb2ee0b537c09fac63ff6f1ba17014349fe3f
shilpa <36531617+shilpathota@users.noreply.github.com>
2019-11-28 18:41:58 +0530

    Add files via upload

---
+++ firstNodejs.js
@@ -0,0 +1,5 @@
+//This is my first node js project
+console.log('This is my first nodejs project')
+
+// type "node firstNodejs.js" in terminal gives the output
+// Output: This is my first nodejs project
